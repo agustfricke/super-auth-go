4c56f3deef9f309d9ec6861ca8648207dd4a26bb
Agust <agustfricke@protonmail.com>
2023-09-22 03:55:36 -0300

    feat register email verify, not tested

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,17 +1,126 @@
 package handlers
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/smtp"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+
+	"github.com/agustfricke/super-auth-go/config"
+	"github.com/agustfricke/super-auth-go/database"
+	"github.com/agustfricke/super-auth-go/models"
 	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
 )
 
+func SendEmail(token string, email string) {
+	secretPassword := config.Config("EMAIL_SECRET_KEY")
+	host := config.Config("HOST")
+	auth := smtp.PlainAuth(
+		"",
+		"agustfricke@gmail.com",
+		secretPassword,
+		"smtp.gmail.com",
+	)
+
+	tmpl, err := template.ParseFiles("/templates/email_template.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	data := struct {
+		Token string
+		Host  string
+	}{
+		Token: token,
+		Host:  host,
+	}
+
+	var bodyContent bytes.Buffer
+	err = tmpl.Execute(&bodyContent, data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	content := fmt.Sprintf("To: %s\r\n"+
+		"Subject: Verify Your Email Address\r\n"+
+		"Content-Type: text/html; charset=utf-8\r\n"+
+		"\r\n"+
+		"%s", email, bodyContent.String())
+
+	err = smtp.SendMail(
+		"smtp.gmail.com:587",
+		auth,
+		"agustfricke@gmail.com",
+		[]string{email},
+		[]byte(content),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+
 func SignUp(c *fiber.Ctx) error {
-  // instancia db
-  // Agarar input desde formulrio
-  // encriptar password con bcrypt
-  // crear usuario en db
-  // mandar email con token
-  // retornar html de aviso para verificar
-	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": "success"})
+  db := database.DB
+
+	time.Sleep(2 * time.Second)
+
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	if name == "" || email == "" || password == "" {
+		return c.Status(fiber.StatusBadRequest).SendString("Missing name, email or password")
+	}
+
+  user := models.User{Name: name, Email: email, Password: string(hashedPassword)}
+	if err := db.Create(&user).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString("Error creating task in database")
+	}
+
+	tokenByte := jwt.New(jwt.SigningMethodHS256)
+
+	now := time.Now().UTC()
+	claims := tokenByte.Claims.(jwt.MapClaims)
+
+	claims["sub"] = user.ID
+	claims["exp"] = now.Add(30).Unix()
+	claims["iat"] = now.Unix()
+	claims["nbf"] = now.Unix()
+
+	tokenString, err := tokenByte.SignedString([]byte(config.Config("JWT_SECRET")))
+
+	if err != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{"status": "fail", "message": fmt.Sprintf("generating JWT Token failed: %v", err)})
+	}
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "token",
+		Value:    tokenString,
+		Path:     "/",
+		MaxAge:   60 * 60,
+		Secure:   false,
+		HTTPOnly: true,
+		Domain:   "localhost",
+	})
+
+  SendEmail(tokenString, email)
+
+  return c.Render("verify", fiber.Map{
+      "user":    user,
+  })
 }
 
 func VerifyEmail(c *fiber.Ctx) error {
