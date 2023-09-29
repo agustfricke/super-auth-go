ee25984d35d36fb337d7029998c4b245501786a5
Agust Fricke <agustfricke@protonmail.com>
2023-09-29 20:59:02 -0300

    google auth

--- handlers/socials.go
+++ handlers/socials.go
@@ -1,7 +1,8 @@
 package handlers
 
 import (
-
+	"github.com/agustfricke/super-auth-go/database"
+	"github.com/agustfricke/super-auth-go/models"
 	"github.com/agustfricke/super-auth-go/socials"
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,22 +15,27 @@ func AuthGoogle(c *fiber.Ctx) error {
 }
 
 func CallbackGoogle(c *fiber.Ctx) error {
-	token, error := socials.ConfigGoogle().Exchange(c.Context(), c.FormValue("code"))
-	if error != nil {
-		panic(error)
-	}
-	email := socials.GetEmail(token.AccessToken)
-  /*
-  if userID match with user.SocialID {
-    Create new user in the database with the info of google
-    generate a new token
-  } else {
-    generate a new token with the info
-  }
-  */
-	return c.Status(200).JSON(fiber.Map{"email": email, "login": true})
-}
+    token, error := socials.ConfigGoogle().Exchange(c.Context(), c.FormValue("code"))
+    if error != nil {
+        panic(error)
+    }
+    googleResponse := socials.GetGoogleResponse(token.AccessToken)
 
+    db := database.DB 
+    var user models.User
+    db.Find(&user, googleResponse.ID)
+
+    if err := db.First(&user, "SocialID = ?", googleResponse.ID).Error; err != nil {
+        user = models.User{
+            SocialID:       googleResponse.ID,
+            Email:          googleResponse.Email,
+        }
+        db.Create(&user)
+      return c.Status(fiber.StatusCreated).JSON(fiber.Map{"status": "created", "user created": user, "google res": googleResponse})
+    } else {
+      return c.Status(fiber.StatusFound).JSON(fiber.Map{"status": "found", "user in db": user, "google res": googleResponse})
+    }
+}
 
 func AuthGitHub(c *fiber.Ctx) error {
   return c.Status(200).JSON(fiber.Map{"state": "success"})
--- socials/google.go
+++ socials/google.go
@@ -25,32 +25,31 @@ func ConfigGoogle() *oauth2.Config {
 	return conf
 }
 
-func GetEmail(token string) string {
-	reqURL, err := url.Parse("https://www.googleapis.com/oauth2/v1/userinfo")
-	if err != nil {
-		panic(err)
-	}
-	ptoken := fmt.Sprintf("Bearer %s", token)
-	res := &http.Request{
-		Method: "GET",
-		URL:    reqURL,
-		Header: map[string][]string{
-			"Authorization": {ptoken}},
-	}
-	req, err := http.DefaultClient.Do(res)
-	if err != nil {
-		panic(err)
-
-	}
-	defer req.Body.Close()
-	body, err := io.ReadAll(req.Body)
-	if err != nil {
-		panic(err)
-	}
-	var data models.GoogleResponse
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		panic(err)
-	}
-	return data.Email
+func GetGoogleResponse(token string) models.GoogleResponse {
+    reqURL, err := url.Parse("https://www.googleapis.com/oauth2/v1/userinfo")
+    if err != nil {
+        panic(err)
+    }
+    ptoken := fmt.Sprintf("Bearer %s", token)
+    res := &http.Request{
+        Method: "GET",
+        URL:    reqURL,
+        Header: map[string][]string{
+            "Authorization": {ptoken}},
+    }
+    req, err := http.DefaultClient.Do(res)
+    if err != nil {
+        panic(err)
+    }
+    defer req.Body.Close()
+    body, err := io.ReadAll(req.Body)
+    if err != nil {
+        panic(err)
+    }
+    var data models.GoogleResponse
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        panic(err)
+    }
+    return data
 }
